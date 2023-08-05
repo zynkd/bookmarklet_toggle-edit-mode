a8c2e28db6d0489621ca907f77cfec0b45fb7f22
zynkd <coding707@mailbox.org>
2023-08-05 16:37:26 +0200

    Added toggle functionality, updated ES5 to ES6.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 javascript: (function () {
-  document.body.contentEditable = true;
+  document.body.contentEditable =
+    document.body.contentEditable === 'true' ? 'false' : 'true';
 
   // Allowing us to click on the anchor text without being redirected
   let links = document.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++) {
-    links[i].removeAttribute('href');
+  for (link of links) {
+    if (links.dataset.hrefBackup !== undefined) {
+      link.href = link.dataset.hrefBackup;
+      delete link.dataset.hrefBackup;
+    } else {
+      link.dataset.hrefBackup = link.getAttribute('href');
+      link.removeAttribute('href');
+    }
   }
 })();
